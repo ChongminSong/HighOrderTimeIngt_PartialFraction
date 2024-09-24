d61795cdb2cbd82c11eaf06336d237813241b9ae
Chongmin Song <51593191+ChongminSong@users.noreply.github.com>
2024-09-24 10:50:49 +1000

    Add bibli info

--- src/PadeExpansion.m
+++ src/PadeExpansion.m
@@ -1,5 +1,24 @@
 function [pcoe, qcoe, r] = PadeExpansion(M,rhoInfty)
 
+% The function is a part of the source code implementing 
+% the high-order implicit time integration schemes in the paper:
+% @misc{oshea2024highorderimplicittimeintegration,
+%       title={A high-order implicit time integration method for linear and nonlinear dynamics with efficient computation of accelerations}, 
+%       author={Daniel O'Shea and Xiaoran Zhang and Shayan Mohammadian and Chongmin Song},
+%       year={2024},
+%       eprint={2409.13397},
+%       archivePrefix={arXiv},
+%       primaryClass={math.NA},
+%       url={https://arxiv.org/abs/2409.13397}, 
+% }
+% 
+% The code is developed by:
+%          Chongmin Song (c.song@unsw.edu.au)
+%          Daniel O'Shea (d.oshea@unsw.edu.au)
+%          Xiaoran Zhang (xiaoran.zhang1@student.unsw.edu.au)  
+% 
+% It is distributed under the MIT License (https://opensource.org/license/mit/)
+
 % mixed-order Padé expansion (L=M-1, M)
 
 % M: order
